d316993d62f1df5d6cef989b77b43d1f1856d188
Anticrafty <piratnovak@seznam.cz>
2018-11-21 16:07:18 +0100

    Dalsi ukol

---
+++ Formular Osoby/Formular Osoby/Osoba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formular_Osoby
+{
+    class Osoba
+    {
+        public string jmeno;
+        public string primeni;
+        public string Email;
+        public DateTime DatumNarozeni;
+    }
+}
