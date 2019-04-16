2d2ab1e1a22ec126ba6a188c5fb186efb59c0437
Jan Novák <piratnovak@seznam.cz>
2019-04-16 17:09:00 +0200

    Yeet my meet

---
+++ Either Mouse/Either Mouse/App.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+
+namespace Either_Mouse
+{
+    /// <summary>
+    /// Interakční logika pro App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private System.Windows.Forms.NotifyIcon _NotifikacniIkona;
+        private bool _BiEmpty;
+        private bool _BiShowed = false;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Closing += Bi_Emptying;
+
+            _NotifikacniIkona = new System.Windows.Forms.NotifyIcon();
+            _NotifikacniIkona.DoubleClick += (s, args) => ShowBi();
+            _NotifikacniIkona.Icon = Either_Mouse.Properties.Resources.ProsteIkona;
+            _NotifikacniIkona.Visible = true;
+
+            VytvorNaklikavaciMenu();
+        }
+        private void VytvorNaklikavaciMenu()
+        {
+            _NotifikacniIkona.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            _NotifikacniIkona.ContextMenuStrip.Items.Add("UnEmpty").Click += (s, e) => ShowBi();
+            _NotifikacniIkona.ContextMenuStrip.Items.Add("Yeet").Click += (s, e) => ThisBiEmpty();
+            
+        }
+
+        private void ThisBiEmpty()
+        {
+            _BiEmpty = true;
+            MainWindow.Close();
+            _NotifikacniIkona.Dispose();
+            _NotifikacniIkona = null;
+        }
+
+        private void ShowBi()
+        {
+            if (_BiShowed)
+            {
+                MainWindow.Hide();
+                _BiShowed = false;
+            }
+            else
+            {
+                if (MainWindow.IsVisible)
+                {
+                    if (MainWindow.WindowState == WindowState.Minimized)
+                    {
+                        MainWindow.WindowState = WindowState.Normal;
+                    }
+                    //MainWindow.Activate();
+                }
+                else
+                {
+                    MainWindow.Show();
+                    _BiShowed = true;
+                }
+            }
+            
+        }
+
+        private void Bi_Emptying(object sender, CancelEventArgs e)
+        {
+            if(!_BiEmpty)
+            {
+                e.Cancel = true;
+                MainWindow.Hide();
+            }
+        }
+    }
+}
---
+++ Either Mouse/Either Mouse/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Either_Mouse
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Zarizeni> zarizenis = new List<Zarizeni>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            NactiZarizeni();
+            vypis_zarizeni();
+        }
+
+        public void NactiZarizeni()
+        {
+            zarizenis.Add(new Zarizeni() { jmeno = "Mys" });
+        }
+        public void vypis_zarizeni()
+        {
+            Zarizeninini.Items.Clear();
+            foreach ( Zarizeni zarizeni in zarizenis)
+            {
+                ComboBoxItem NCBI = new ComboBoxItem();
+                NCBI.Content = new TextBlock() { Text = zarizeni.jmeno };
+                Zarizeninini.Items.Add(NCBI);
+            }
+        }
+        private void novy_vybrany_zarizeni(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            ComboBoxItem urceny = Zarizeninini.SelectedItem as ComboBoxItem;
+            if(urceny != null)
+            { 
+                TextBlock urceny_text = urceny.Content as TextBlock;
+                string urceny_jmeno = urceny_text.Text;
+                foreach (Zarizeni zarizeni in zarizenis)
+                {
+                    if(zarizeni.jmeno == urceny_jmeno)
+                    {
+                        Citlivost.Value = zarizeni.Citlivost;
+                        Dvojklik.Value = zarizeni.Click;
+                        Scroll.Value = zarizeni.Scroll;
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            zarizenis.Add(new Zarizeni { jmeno = Name.Text });
+            vypis_zarizeni();
+            Uloz_zarizeni();
+            Name.Text = null;
+        }
+
+        public void Uloz_zarizeni()
+        {
+            
+        }
+
+        private void Uloz_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem urceny = Zarizeninini.SelectedItem as ComboBoxItem;
+            if (urceny != null)
+            {
+                TextBlock urceny_text = urceny.Content as TextBlock;
+                string urceny_jmeno = urceny_text.Text;
+                foreach (Zarizeni zarizeni in zarizenis)
+                {
+                    if (zarizeni.jmeno == urceny_jmeno)
+                    {
+                        zarizeni.Citlivost = int.Parse(Citlivost.Value.ToString());
+                        zarizeni.Click = int.Parse(Dvojklik.Value.ToString());
+                        zarizeni.Scroll = int.Parse(Scroll.Value.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
